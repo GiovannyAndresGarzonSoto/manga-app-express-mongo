c864a0367908f395e4b1811cf4d19a8b609c60bb
Giovanny <giovannygarzonsoto@gmail.com>
2026-04-21 02:37:06 -0400

    atlas mongo port removed

--- src/database.ts
+++ src/database.ts
@@ -3,7 +3,7 @@ import mongoose, {connect} from 'mongoose'
 mongoose.set('strictQuery', true)
 export const connectDatabase = async() => {
     try{
-        await connect(`mongodb+srv://${process.env.ATLAS_URI}@cluster0.jfmq85b.mongodb.net:27017/manga-app?retryWrites=true&w=majority`)
+        await connect(`mongodb+srv://${process.env.ATLAS_URI}@cluster0.jfmq85b.mongodb.net/manga-app?retryWrites=true&w=majority`)
         console.log('Database is connected')
     }catch(err){
         console.log(err)
