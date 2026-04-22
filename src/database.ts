52a930160069fa91d6a4d76c596468d25f22e888
Giovanny <giovannygarzonsoto@gmail.com>
2026-04-21 22:02:51 -0400

    updates config

--- src/database.ts
+++ src/database.ts
@@ -1,6 +1,6 @@
 import mongoose, {connect} from 'mongoose' 
 
-mongoose.set('strictQuery', true)
+mongoose.set('strictQuery', false)
 export const connectDatabase = async() => {
     const uri = `mongodb://${process.env.ATLAS_URI}@ac-gh5w1ew-shard-00-00.jfmq85b.mongodb.net,ac-gh5w1ew-shard-00-01.jfmq85b.mongodb.net:27017,ac-gh5w1ew-shard-00-02.jfmq85b.mongodb.net:27017/?ssl=true&replicaSet=atlas-j5r3s9-shard-0&authSource=admin&appName=Cluster0`
     try{
