2ae6028de5cc3028246e0e95b66e1ac9313b6bf4
Giovanny <giovannygarzonsoto@gmail.com>
2026-04-21 22:05:37 -0400

    updates config

--- src/database.ts
+++ src/database.ts
@@ -2,7 +2,7 @@ import mongoose, {connect} from 'mongoose'
 
 mongoose.set('strictQuery', false)
 export const connectDatabase = async() => {
-    const uri = `mongodb://${process.env.ATLAS_URI}@ac-gh5w1ew-shard-00-00.jfmq85b.mongodb.net,ac-gh5w1ew-shard-00-01.jfmq85b.mongodb.net:27017,ac-gh5w1ew-shard-00-02.jfmq85b.mongodb.net:27017/?ssl=true&replicaSet=atlas-j5r3s9-shard-0&authSource=admin&appName=Cluster0`
+    const uri = `mongodb//${process.env.ATLAS_URI}@ac-gh5w1ew-shard-00-00.jfmq85b.mongodb.net,ac-gh5w1ew-shard-00-01.jfmq85b.mongodb.net,ac-gh5w1ew-shard-00-02.jfmq85b.mongodb.net/?ssl=true&replicaSet=atlas-j5r3s9-shard-0&authSource=admin&appName=Cluster0`
     try{
         await connect(uri)
         console.log('Database is connected')
